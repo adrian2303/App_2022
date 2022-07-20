723ef5ec6a23f881a181db67872708558175ec1b
ahamada <ahamada>
2022-07-20 10:13:13 +0200

    Vytvorenie layoutu pre aplikaciu, vlozenie EditTextov a tlacidiel

--- app/src/main/java/com/example/android_aplikacia_2022/MainActivity.kt
+++ app/src/main/java/com/example/android_aplikacia_2022/MainActivity.kt
@@ -2,10 +2,36 @@ package com.example.android_aplikacia_2022
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.android_aplikacia_2022.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var appDb: ApplicationDatabase
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnWriteData.setOnClickListener {
+
+            writeData()
+        }
+
+        binding.btnReadData.setOnClickListener {
+
+            readData()
+        }
+    }
+
+    private  fun writeData(){
+
+
+    }
+
+    private  fun readData(){
+
+
     }
 }
